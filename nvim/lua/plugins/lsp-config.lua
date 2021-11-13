f5bd6af6c104beabde9a3ddb7cbbfbfbea835e74
mitchthorson <mitch.thorson@protonmail.com>
2021-11-12 20:10:03 -0500

    setup language servers for neovim

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,1 +1,6 @@
 vim.api.nvim_set_keymap('n', '<silent> gd', 'v:vim.lsp.buf.definition()', {noremap = true})
+
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.cssls.setup{}
+require'lspconfig'.html.setup{}
+
