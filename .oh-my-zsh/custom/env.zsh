84eb33d873616ad18e2f2112f037b508fede6cc8
mitchthorson <mitch.thorson@protonmail.com>
2021-11-24 19:23:38 -0500

    update gitignore setup, add zsh_custom

---
+++ .oh-my-zsh/custom/env.zsh
@@ -0,0 +1,5 @@
+TYPEWRITTEN_PROMPT_LAYOUT="singleline_verbose"
+TYPEWRITTEN_SYMBOL="🌙 >"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+export GPG_TTY=$(tty)
