7ff260ba0da8ae3de595474d03f318184ad2ca78
mitchthorson <mitch.thorson@protonmail.com>
2022-01-24 22:15:23 -0500

    fix polybar launch on laptop

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -5,13 +5,14 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-if [ -z $DESKTOP_DUAL_DISPLAY ]; then
+if [ -n "$DESKTOP_DUAL_DISPLAY" ]; then
 	echo "launching dual monitor mode"
 	echo "---" | tee -a /tmp/polybar1.log 
 	polybar dual_main 2>&1 | tee -a /tmp/polybar1.log & disown
 	polybar dual_secondary 2>&1 | tee -a /tmp/polybar2.log & disown
 else
 	# Launch bar1 and bar2
+	echo "launching single monitor mode"
 	echo "---" | tee -a /tmp/polybar1.log 
 	polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
 fi
