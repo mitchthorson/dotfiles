b675270097730f37ebe7d08468df674951507177
mitchthorson <mitch.thorson@protonmail.com>
2022-02-10 10:48:22 -0500

    rework nvim-tree

--- nvim/.config/nvim/lua/nvim-tree-config/init.lua
+++ nvim/.config/nvim/lua/nvim-tree-config/init.lua
@@ -1,15 +1,23 @@
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_refresh_wait = 250
 vim.g.nvim_tree_quit_on_open = 1
-vim.cmd[[let g:nvim_tree_show_icons = {
-	\ 'git': 0,
-	\ 'folders': 1,
-	\ 'files': 1,
-	\ 'folder_arrows': 0,
-\ }]]
+vim.g.nvim_tree_show_icons = {
+	git = 0,
+	folders = 1,
+	files = 0,
+	folder_arrows = 0
+}
+vim.g.nvim_tree_icons = {
+	folder = {
+		arrow_open = '',
+		arrow_closed = '',
+		default = '',
+		open = ''
+	}
+}
 require'nvim-tree'.setup {
-	open_on_setup    = true,
-	auto_close       = true,
+	open_on_setup    = false,
+	auto_close       = false,
 	view = {
 		width = 30,
 		auto_resize = true
@@ -18,3 +26,5 @@ require'nvim-tree'.setup {
 
 
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+-- vim.cmd(string.format("hi NvimTreeIndentMarker guifg=%s", "red"))
+-- vim.cmd("hi NvimTreeIndentMarker guifg=red")
