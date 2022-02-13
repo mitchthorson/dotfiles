e6dca6452c046b38303b344e47b8840ec9cf34eb
mitchthorson <mitch.thorson@protonmail.com>
2022-02-13 12:21:51 -0500

    colors for linux

--- zsh/.zsh/shared.zsh
+++ zsh/.zsh/shared.zsh
@@ -16,12 +16,30 @@ autoload -z edit-command-line
 zle -N edit-command-line
 bindkey -M vicmd v "edit-command-line"
 
+# color key
+# 0 black
+# 1 red
+# 2 green
+# 3 yellow
+# 4 blue
+# 5 magenta
+# 6 cyan
+# 7 white
+# 8 light black
+# 9 light red
+# 10 light green
+# 11 light yellow
+# 12 light blue
+# 13 light magenta
+# 14 light cyan
+# 15 light white
+
 # prompt
 autoload -Uz vcs_info
 precmd () { vcs_info }
 setopt prompt_subst
-zstyle ':vcs_info:git:*' formats ' %F{white}on %F{yellow} %b'
-PROMPT='%F{magenta}%n %F{white}in %F{cyan} %~${vcs_info_msg_0_} %F{white}❯ '
+zstyle ':vcs_info:git:*' formats ' %F{white}on %F{14} %b'
+PROMPT='%F{9}%n %F{white}in %F{13} %~${vcs_info_msg_0_} %F{3}❯ '
 # RPROMPT='%F{blue}%* %F{green}%(?.﫟.)'
 
 # aliases
@@ -33,8 +51,8 @@ alias ll="ls -lah"
 alias zrc="$EDITOR $HOME/dotfiles/zsh/.zshrc_linux"
 
 # plugins
-# source "$HOME/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
-# source "$HOME/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "$HOME/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$HOME/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
 # nvm setup
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
