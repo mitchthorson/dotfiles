7206d594e984ba0f7a6ad329cf5e2bd640134c5b
mitchthorson <mitch.thorson@protonmail.com>
2022-02-16 10:29:00 -0500

    clean up lua code

--- nvim/.config/nvim/lua/statusline.lua
+++ nvim/.config/nvim/lua/statusline.lua
@@ -4,10 +4,6 @@
 
 -- heavy inspiration from this blog: https://nuxsh.is-a.dev/blog/custom-nvim-statusline.html
 
-local fn = vim.fn
-local api = vim.api
-local cmd = vim.cmd
-
 local modes = {
   ["n"] = "NORMAL",
   ["no"] = "NORMAL",
@@ -32,12 +28,12 @@ local modes = {
 }
 
 local function mode()
-  local current_mode = api.nvim_get_mode().mode
+  local current_mode = vim.api.nvim_get_mode().mode
   return string.format("%s", modes[current_mode]):upper()
 end
 
 local function filepath()
-  local fpath = fn.fnamemodify(fn.expand "%", ":~:.:h")
+  local fpath = vim.fn.fnamemodify(vim.fn.expand "%", ":~:.:h")
   if fpath == "" or fpath == "." then
       return ""
   end
@@ -46,7 +42,7 @@ local function filepath()
 end
 
 local function filename()
-  local fname = fn.expand "%:t"
+  local fname = vim.fn.expand "%:t"
   if fname == "" then
       return ""
   end
@@ -122,7 +118,7 @@ function Statusline.short()
   return "%#StatusLine#   NvimTree"
 end
 
-api.nvim_exec([[
+vim.api.nvim_exec([[
   augroup Statusline
   au!
   au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline.active()
