216692f0c89a6581417cea6e22cf2d934d3e2b71
mitchthorson <mitchell@mitchellthorson.com>
2023-03-30 23:17:17 -0400

    reorder nvim plugins

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -27,23 +27,12 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	'sbdchd/neoformat',
-	{
-		'nvim-treesitter/nvim-treesitter',
-	},
-	'Mofiqul/dracula.nvim',
-	'ellisonleao/gruvbox.nvim',
-	'shaunsingh/nord.nvim',
-	'folke/tokyonight.nvim',
+	'clojure-vim/vim-jack-in',
 	{ "catppuccin/nvim", name = "catppuccin" },
-	'rose-pine/neovim',
-	'windwp/nvim-ts-autotag',
-	'p00f/nvim-ts-rainbow',
-	'windwp/nvim-autopairs',
-	'nvim-lua/plenary.nvim',
-	'nvim-telescope/telescope.nvim',
-	'nvim-telescope/telescope-file-browser.nvim',
-	'neovim/nvim-lspconfig',
+	'ellisonleao/gruvbox.nvim',
+	'folke/tokyonight.nvim',
+	'github/copilot.vim', -- 🤖
+	'hkupty/iron.nvim',
 	{
 		'hrsh7th/nvim-cmp',
 		dependencies = {
@@ -53,29 +42,40 @@ local plugins = {
 
 		}
 	},
-	'L3MON4D3/LuaSnip',
-	'saadparwaiz1/cmp_luasnip',
-	'onsails/lspkind-nvim',
-	'numToStr/Comment.nvim',
-	'lewis6991/gitsigns.nvim',
-	'tpope/vim-fugitive',
-	'tpope/vim-rhubarb',
 	'jalvesaq/Nvim-R',
 	'kyazdani42/nvim-tree.lua',
-	{'vimwiki/vimwiki', init=require("vimwiki-config").init},
-	--currently testing out some clojure plugins
-	-- 'tpope/vim-fireplace'
-	'Olical/conjure',
-	'tpope/vim-dispatch', -- dependency for vim-jack-in
-	'clojure-vim/vim-jack-in',
-	'tidalcycles/vim-tidal', -- tidal cycles music software
-	'hkupty/iron.nvim',
-	'tpope/vim-sleuth',
+	'lewis6991/gitsigns.nvim',
+	'L3MON4D3/LuaSnip',
+	'Mofiqul/dracula.nvim',
+	'neovim/nvim-lspconfig',
+	{
+		'nvim-treesitter/nvim-treesitter',
+	},
+	'numToStr/Comment.nvim',
+	'nvim-lua/plenary.nvim',
+	'nvim-telescope/telescope.nvim',
+	'nvim-telescope/telescope-file-browser.nvim',
+	'onsails/lspkind-nvim',
+	'p00f/nvim-ts-rainbow',
 	{'quarto-dev/quarto-nvim', dependencies = {
 		'jmbuhr/otter.nvim',
 		'neovim/nvim-lspconfig'
 	}},
-	'github/copilot.vim',
+	'rose-pine/neovim',
+	'saadparwaiz1/cmp_luasnip',
+	'sbdchd/neoformat',
+	'shaunsingh/nord.nvim',
+	--currently testing out some clojure plugins
+	-- 'Olical/conjure',
+	-- 'tidalcycles/vim-tidal', -- tidal cycles music software
+	-- 'tpope/vim-fireplace'
+	'tpope/vim-fugitive',
+	'tpope/vim-rhubarb',
+	'tpope/vim-dispatch', -- dependency for vim-jack-in
+	'tpope/vim-sleuth',
+	{'vimwiki/vimwiki', init=require("vimwiki-config").init},
+	'windwp/nvim-ts-autotag',
+	'windwp/nvim-autopairs',
 }
 
 require('lazy').setup(plugins)
