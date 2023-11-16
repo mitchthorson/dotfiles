850c00baca480253bc1eebf170e0305f23e543b7
mitchthorson <mitchell@mitchellthorson.com>
2023-11-16 09:29:12 -0500

    add format shortcut

--- nvim/.config/nvim/lua/neoformat/init.lua
+++ nvim/.config/nvim/lua/neoformat/init.lua
@@ -2,4 +2,6 @@ return function()
   vim.g.neoformat_enabled_python = {'black'}
 	vim.g.neoformat_try_node_exe = true
 	vim.g.neoformat_enabled_javascript = {'prettier'}
+  vim.api.nvim_set_keymap('n', '<leader>nf', ':Neoformat<CR>', {noremap = true})
 end
+
