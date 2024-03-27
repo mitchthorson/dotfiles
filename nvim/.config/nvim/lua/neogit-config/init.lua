cedeac7d3e3e43903a35ced7c236f26ad1c0c148
mitchthorson <mitchell@mitchellthorson.com>
2024-03-27 17:21:05 -0400

    Add neogit

---
+++ nvim/.config/nvim/lua/neogit-config/init.lua
@@ -0,0 +1,13 @@
+local neogit = require("neogit")
+
+vim.keymap.set("n", "<leader>gs", neogit.open, { silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>gc", ":Neogit commit<CR>", { silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>gp", ":Neogit pull<CR>", { silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>gP", ":Neogit push<CR>", { silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>", { silent = true, noremap = true })
+
+vim.keymap.set("n", "<leader>gB", ":G blame<CR>", { silent = true, noremap = true })
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -189,6 +189,27 @@ local plugins = {
   "tpope/vim-dispatch", -- dependency for vim-jack-in
   -- 'tpope/vim-sleuth',
   {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+    },
+    config = function ()
+      require('neogit').setup({
+        integrations = {
+          diffview = true,
+          telescope = true,
+        }
+      })
+      require("neogit-config")
+      
+    end
+  },
+  {
     "vimwiki/vimwiki",
     init = require("vimwiki-config"),
   },
