a991d87be28180b5dd723374c3c23ff6f6c1bcca
mitchthorson <mitchell@mitchellthorson.com>
2024-06-06 23:48:32 -0400

    Add gnome-extensions script

---
+++ scripts/ubuntu/gnome-extensions.sh
@@ -0,0 +1,39 @@
+# Turn off default Ubuntu extensions
+gnome-extensions disable tiling-assistant@ubuntu.com
+gnome-extensions disable ubuntu-appindicators@ubuntu.com
+gnome-extensions disable ubuntu-dock@ubuntu.com
+gnome-extensions disable ding@rastersoft.com
+
+# Install new extensions
+# FIXME: Install these manually to avoid needing the pop-up confirmation during the installation process
+gext install tactile@lundal.io
+gext install just-perfection-desktop@just-perfection
+gext install space-bar@luchrioh
+
+# Compile gsettings schemas in order to be able to set them
+sudo cp ~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/schemas/org.gnome.shell.extensions.space-bar.gschema.xml /usr/share/glib-2.0/schemas/
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+# Configure Tactile
+gsettings set org.gnome.shell.extensions.tactile col-0 1
+gsettings set org.gnome.shell.extensions.tactile col-0 1
+gsettings set org.gnome.shell.extensions.tactile col-1 2
+gsettings set org.gnome.shell.extensions.tactile col-2 1
+gsettings set org.gnome.shell.extensions.tactile col-3 0
+gsettings set org.gnome.shell.extensions.tactile row-0 1
+gsettings set org.gnome.shell.extensions.tactile row-1 1
+gsettings set org.gnome.shell.extensions.tactile gap-size 0
+
+# Configure Just Perfection
+gsettings set org.gnome.shell.extensions.just-perfection animation 2
+gsettings set org.gnome.shell.extensions.just-perfection dash-app-running true
+gsettings set org.gnome.shell.extensions.just-perfection workspace true
+gsettings set org.gnome.shell.extensions.just-perfection workspace-popup false
+
+# Configure Space Bar
+gsettings set org.gnome.shell.extensions.space-bar.behavior smart-workspace-names false
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-activate-workspace-shortcuts false
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-move-to-workspace-shortcuts true
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts open-menu "@as []"
